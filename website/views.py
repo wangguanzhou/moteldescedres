a60d6a8c0c69ab653bb4ff259401d723c5aa012c
Guanzhou Wang <tawayee@gamil.com>
2017-08-11 21:46:22 -0400

    ..

--- website/views.py
+++ website/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'homepage.html', {})
+    context = {}
+    if request.is_ajax():
+        reserveData = {}
+        reserveData['clientName'] = request.POST['clientName']
+        reserveData['phoneNumber'] = request.POST['phoneNumber']
+        context['reserveData'] = reserveData
+        return render(request, 'reserveresponse.html', context)
+        pass;
+    else:
+        return render(request, 'homepage.html', context)
