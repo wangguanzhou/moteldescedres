fafa22800ba955f8ee737363250322a79394368a
Guanzhou Wang <tawayee@gamil.com>
2017-09-04 15:21:31 -0400

    add Nexmo SMS function

--- website/views.py
+++ website/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+import urllib
+from urllib.request import urlopen
 from .forms import *
 
 # Create your views here.
@@ -13,6 +16,10 @@ def reserve(request):
         reserveForm = ReserveForm(request.POST)
         context['reserveForm'] = reserveForm
 
+        smsNotification = 'There is a new reservation via moteldescedres.ca. Please check your email.'
+        sendSMSviaNexmo(smsNotification)
+
+
         return render(request, 'reserveresponse.html', context)
 
     else:
@@ -23,3 +30,23 @@ def reserve(request):
 
 def showrooms(request):
     return render(request, 'rooms.html', {})
+
+
+def sendSMSviaNexmo(smsContent):
+    params = {
+            'api_key': '8f27294a',
+            'api_secret': 'f44647eee9a437f0',
+            'to': '15147757799',
+            'from': '12264063982',
+            'text': smsContent
+            }
+
+    url = 'https://rest.nexmo.com/sms/json?' + urllib.parse.urlencode(params)
+    request = urllib.request.Request(url)
+    request.add_header('Accept', 'application/json')
+    response = urllib.request.urlopen(request)
+    if response.code == 200 :
+        print('SMS notification success')
+    else:
+        print('SMS notification failure')
+
